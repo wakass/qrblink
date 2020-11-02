d47a8407f86f46c656111ca17669f27319b116c5
waka <p.c.spruijtenburg@utwente.nl>
2020-11-02 23:39:03 +0100

    QR code generating and displaying on screen with custom shader

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ var $ = require('jquery');
 let camera, scene, renderer;
 let geometry, material, mesh;
 
+drawQR();
 init();
+
 // animate();
 renderer.render(scene,camera);
+
+function drawQR() {
+	var QRC = qrcodegen.QrCode;
+	var qr0 = QRC.encodeText("Hello, world!", QRC.Ecc.MEDIUM);
+
+	const canvas = $('#myawesomecanvas')[0]; //Get the actual dom element by indexing into the jquery array
+
+	qr0.drawCanvas(1, 0, canvas);
+
+}
 
 function init() {
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
@@ -15,13 +27,20 @@ function init() {
 
 	scene = new THREE.Scene();
 
+	const canvas = $('#myawesomecanvas')[0];
+
+	const qrcodeTexture = new THREE.CanvasTexture(canvas);
+	qrcodeTexture.magFilter = THREE.NearestFilter;
 	
-	const plane = new THREE.PlaneGeometry( 1, 1, 10,10 );
+
+	const plane = new THREE.PlaneGeometry( 1.5, 1.5, 10,10 );
 	
 	shaderMaterial = new THREE.ShaderMaterial( {
 			uniforms: {
 				u_time: { value: 1.0 },
-				u_resolution: { value: new THREE.Vector2(window.innerWidth,window.innerHeight) }
+				u_resolution: { value: new THREE.Vector2(window.innerWidth,window.innerHeight) },
+				u_qrcodeTexture: { type: "t", value: qrcodeTexture }
+
 			}
 		}
 	);
@@ -29,11 +48,15 @@ function init() {
 
 	shaderMaterial.fragmentShader = `
 	uniform vec2 u_resolution;
+	uniform sampler2D u_qrcodeTexture;
+
 	void main() {
 		
 		vec2 st = gl_FragCoord.xy/u_resolution.xy;
 
-		gl_FragColor =vec4(sin(st.x*10.0*3.13),sin(st.y*10.0*3.14),0.0,0.0);
+		vec4 color = texture2D(u_qrcodeTexture, st);
+
+		gl_FragColor = vec4(color.xyz,0.0);
 	}	
 	`
 
